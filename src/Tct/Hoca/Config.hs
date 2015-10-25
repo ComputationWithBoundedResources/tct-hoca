598eccb91f9a480f809321e47d67ca416366a493
mschaper <michael.schaper@uibk.ac.at>
2015-10-25 17:07:09 +0100

    use Declared

--- src/Tct/Hoca/Config.hs
+++ src/Tct/Hoca/Config.hs
@@ -1,27 +1,27 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Tct.Hoca.Config where
 
 import Control.Monad.Error (catchError)
 import Tct.Core
-import Tct.Core.Main
+import qualified Tct.Core.Data as T
 import qualified Tct.Hoca.Strategies as S
 import qualified Tct.Trs.Strategy.Runtime as RC
 
-import qualified Tct.Core.Parse as TP
-import qualified Tct.Core.Common.Parser as TP
 import Tct.Trs.Data (TrsStrategy)
 import Tct.Trs.Declarations () -- TODO
 
 import Tct.Hoca.Types
 
 
-fun :: Argument Optional (Maybe String)
-fun = optional (some string) Nothing
-      `withName` "function"
-      `withHelp` ["The analysed ML function.",  "It defaults to the last defined function."]
+fun :: Argument 'Optional (Maybe String)
+fun = some (string "function" ["The analysed ML function.",  "It defaults to the last defined function."])
+  `optional` Nothing
 
-tctStrategy :: Argument Required TrsStrategy
-tctStrategy = strat `withName` "tct-strategy"
-                       `withHelp` ["The TRS strategy to apply after a successfull transformation."]
+tctStrategy :: Argument 'Required TrsStrategy
+tctStrategy = strat "tct-strategy" ["The TRS strategy to apply after a successfull transformation."]
+
+instance T.DefaultDeclared ML ML where
+  defaultDecls = [hocaDefault, hocaDefunctionalize]
 
 hocaDefault :: StrategyDeclaration ML ML
 hocaDefault = SD $ strategy "hoca" (fun, tctStrategy) $ \ mn solve ->
