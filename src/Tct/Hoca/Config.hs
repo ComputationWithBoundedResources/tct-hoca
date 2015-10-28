e2a24b148651167d8e371f50fd15a6d6159183d6
mschaper <michael.schaper@uibk.ac.at>
2015-10-28 10:22:16 +0100

    declared without overlappable

--- src/Tct/Hoca/Config.hs
+++ src/Tct/Hoca/Config.hs
@@ -12,26 +12,41 @@ import Tct.Trs.Declarations () -- TODO
 
 import Tct.Hoca.Types
 
+import qualified Tct.Core.Data as T
+
+import Tct.Trs.Data
+-- import qualified Tct.Core.Data as T
+
+-- import Tct.Trs.Data
+-- import Tct.Trs.Strategy.Web
+
+-- instance T.Declared TrsProblem TrsProblem where
+--   decls = [T.SD webDeclaration]
 
 fun :: Argument 'Optional (Maybe String)
 fun = some (string "function" ["The analysed ML function.",  "It defaults to the last defined function."])
   `optional` Nothing
 
-tctStrategy :: Argument 'Required TrsStrategy
+-- tctStrategy :: T.Declared TrsProblem TrsProblem => Argument 'Required TrsStrategy
+tctStrategy :: T.Declared TrsProblem TrsProblem => Argument 'Required TrsStrategy
 tctStrategy = strat "tct-strategy" ["The TRS strategy to apply after a successfull transformation."]
 
-instance T.DefaultDeclared ML ML where
+instance (T.Declared ML ML , T.Declared TrsProblem TrsProblem) => T.DefaultDeclared ML ML where
   defaultDecls = [hocaDefault, hocaDefunctionalize]
 
-hocaDefault :: StrategyDeclaration ML ML
-hocaDefault = SD $ strategy "hoca" (fun, tctStrategy) $ \ mn solve ->
-  S.hoca mn .>>> solve .>>> abort
+hocaDefault :: (T.Declared ML ML, T.Declared TrsProblem TrsProblem) => StrategyDeclaration ML ML
+-- hocaDefault = SD $ strategy "hoca" (fun, tctStrategy) $ \ mn solve ->
+  -- S.hoca mn .>>> solve .>>> abort
+hocaDefault = SD $ strategy "hoca" (OneTuple tctStrategy) $ \ solve ->
+  S.hoca Nothing .>>> solve .>>> abort
 
-hocaDefunctionalize :: StrategyDeclaration ML ML
-hocaDefunctionalize = SD $ strategy "defunctionalize" (fun, tctStrategy) $ \ mn solve ->
-  S.hocaDefunctionalize mn .>>> solve .>>> abort
+hocaDefunctionalize :: (T.Declared ML ML, T.Declared TrsProblem TrsProblem) => StrategyDeclaration ML ML
+-- hocaDefunctionalize = SD $ strategy "defunctionalize" (fun, tctStrategy) $ \ mn solve ->
+  -- S.hocaDefunctionalize mn .>>> solve .>>> abort
+hocaDefunctionalize = SD $ strategy "defunctionalize" (OneTuple tctStrategy) $ \ solve ->
+  S.hocaDefunctionalize Nothing .>>> solve .>>> abort
 
-hocaConfig :: TctConfig ML
+hocaConfig :: (T.Declared ML ML, T.Declared TrsProblem TrsProblem) => TctConfig ML
 hocaConfig =
   (defaultTctConfig parser) { strategies = [hocaDefault, hocaDefunctionalize]
                             , defaultStrategy = S.hoca Nothing .>>> RC.runtime .>>> abort }
--- tct-hoca/Main.hs
+++ tct-hoca/Main.hs
@@ -1,7 +1,20 @@
 module Main where
 
 import Tct.Hoca.Config (hocaConfig)
+import Tct.Hoca.Types
 import Tct.Core.Main (tct3)
+
+import qualified Tct.Core.Data as T
+
+import Tct.Trs.Data
+import Tct.Trs.Strategy.Web
+
+
+instance T.Declared TrsProblem TrsProblem where
+  decls = [T.SD webDeclaration]
+
+instance T.Declared ML ML where
 
 main :: IO ()
 main = tct3 hocaConfig
+
