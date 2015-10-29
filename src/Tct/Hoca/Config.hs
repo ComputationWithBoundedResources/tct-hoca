028fd1d686ce0440834f36ffee79546889917435
Martin Avanzini <martin.avanzini@uibk.ac.at>
2015-10-29 20:17:34 +0100

    added suitable Declared contexts

--- src/Tct/Hoca/Config.hs
+++ src/Tct/Hoca/Config.hs
@@ -10,21 +10,21 @@ import           Tct.Trs.Data
 import qualified Tct.Trs.Strategy.Runtime as RC
 
 
-hocaDeclarations :: T.Declared TrsProblem TrsProblem => [StrategyDeclaration ML ML]
+hocaDeclarations :: (T.Declared ML ML, T.Declared TrsProblem TrsProblem) => [StrategyDeclaration ML ML]
 hocaDeclarations = [ hocaDefault, hocaDefunctionalize ]
 
 fun :: Argument 'Optional (Maybe String)
 fun = some (string "function" ["The analysed ML function.",  "It defaults to the last defined function."])
   `optional` Nothing
 
-tctStrategy :: T.Declared TrsProblem TrsProblem => Argument 'Required TrsStrategy
+tctStrategy :: (T.Declared ML ML, T.Declared TrsProblem TrsProblem) => Argument 'Required TrsStrategy
 tctStrategy = strat "tct-strategy" ["The TRS strategy to apply after a successfull transformation."]
 
-hocaDefault :: T.Declared TrsProblem TrsProblem => StrategyDeclaration ML ML
+hocaDefault :: (T.Declared ML ML, T.Declared TrsProblem TrsProblem) => StrategyDeclaration ML ML
 hocaDefault = SD $ strategy "hoca" (fun, tctStrategy) $ \ mn solve ->
   S.hoca mn .>>> solve .>>> abort
 
-hocaDefunctionalize :: T.Declared TrsProblem TrsProblem => StrategyDeclaration ML ML
+hocaDefunctionalize :: (T.Declared ML ML, T.Declared TrsProblem TrsProblem) => StrategyDeclaration ML ML
 hocaDefunctionalize = SD $ strategy "defunctionalize" (fun, tctStrategy) $ \ mn solve ->
   S.hocaDefunctionalize mn .>>> solve .>>> abort
 
